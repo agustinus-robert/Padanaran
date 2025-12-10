5b52a87395da95127b59fbdb0b91b5d35ce1d7f0
Robert <agustinus2h@gmail.com>
2025-12-10 17:30:35 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,9 +23,9 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable()->index();
             $table->string('profile_photo_path', 2048)->nullable();
-            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete()->index();
-            $table->foreignId('deleted_by')->nullable()->constrained('users')->nullOnDelete()->index();
-            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete()->index();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('updated_by')->nullable()->constrained('users')->nullOnDelete();
             $table->softDeletesTz();
             $table->timestampsTz();
         });
