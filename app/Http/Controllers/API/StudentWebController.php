8c50e3173b76cffcd0b9e0a696d8cf1cebc3bef7
Robert <agustinus2h@gmail.com>
2025-12-17 16:16:51 +0700

    up

--- app/Http/Controllers/API/StudentWebController.php
+++ app/Http/Controllers/API/StudentWebController.php
@@ -15,33 +15,43 @@ class StudentWebController extends Controller
         $search  = $request->get('search');
         $gradeId = userGrades();
 
-        $cacheKey = "students_api:"
+        try {
+            $cacheKey = "students_api:"
             . "grade={$gradeId}:"
             . "search=" . ($search ?: 'null') . ":"
             . "trashed=" . ($trashed ?: 'false');
 
-        $students = Cache::tags(['students'])->remember($cacheKey, now()->addMinutes(60), function () use ($trashed, $search, $gradeId) {
-            Log::info('sedang membuat cache');
-            $std = Student::with('user')
-                ->where('grade_id', $gradeId)
-                ->when($search, fn($query) => $query->whereHas('user', fn($q) => $q->where('name', 'like', "%{$search}%")))
-                ->when($trashed, fn($query) => $query->onlyTrashed())
-                ->orderByDesc('id')
-                ->get(['id', 'user_id']);
+            $students = Cache::tags(['students'])->remember($cacheKey, now()->addMinutes(60), function () use ($trashed, $search, $gradeId) {
+                Log::info('sedang membuat cache');
+                $std = Student::with('user')
+                    ->where('grade_id', $gradeId)
+                    ->when($search, fn($query) => $query->whereHas('user', fn($q) => $q->where('name', 'like', "%{$search}%")))
+                    ->when($trashed, fn($query) => $query->onlyTrashed())
+                    ->orderByDesc('id')
+                    ->get(['id', 'user_id']);
 
-            Log::info('cache berhasil dibuat');
-            return $std;
-        });
+                Log::info('cache berhasil dibuat');
+                return $std;
+            });
 
-        $students = $students->map(fn($student) => [
-            'id' => $student->id,
-            'name' => $student->user->name ?? null,
-        ]);
+            $students = $students->map(fn($student) => [
+                'id' => $student->id,
+                'name' => $student->user->name ?? null,
+            ]);
 
-        Log::info('cache di load');
-        return response()->json([
-            'data' => $students,
-            'count' => $students->count(),
-        ]);
+            Log::info('cache di load');
+            return response()->json([
+                'data' => $students,
+                'count' => $students->count(),
+            ]);
+        } catch (\Throwable $th) {
+            Log::error([
+                'error' => $th->getMessage()
+            ]);
+
+            throw $th;
+        }
+
+
     }
 }
