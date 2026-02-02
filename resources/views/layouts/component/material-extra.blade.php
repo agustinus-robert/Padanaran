24b5432f8c1f556ee11832e654fb80b0bd8b4ff7
Backend2 <agustinus2h@gmail.com>
2026-02-03 05:22:43 +0700

    perfect scroll windows

--- resources/views/layouts/component/material-extra.blade.php
+++ resources/views/layouts/component/material-extra.blade.php
@@ -213,88 +213,88 @@
     });
 
 
-    var ctx2 = document.getElementById("chart-line").getContext("2d");
+    // var ctx2 = document.getElementById("chart-line").getContext("2d");
 
-    new Chart(ctx2, {
-      type: "line",
-      data: {
-        labels: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
-        datasets: [{
-          label: "Sales",
-          tension: 0,
-          borderWidth: 2,
-          pointRadius: 3,
-          pointBackgroundColor: "#43A047",
-          pointBorderColor: "transparent",
-          borderColor: "#43A047",
-          backgroundColor: "transparent",
-          fill: true,
-          data: [120, 230, 130, 440, 250, 360, 270, 180, 90, 300, 310, 220],
-          maxBarThickness: 6
+    // new Chart(ctx2, {
+    //   type: "line",
+    //   data: {
+    //     labels: ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+    //     datasets: [{
+    //       label: "Sales",
+    //       tension: 0,
+    //       borderWidth: 2,
+    //       pointRadius: 3,
+    //       pointBackgroundColor: "#43A047",
+    //       pointBorderColor: "transparent",
+    //       borderColor: "#43A047",
+    //       backgroundColor: "transparent",
+    //       fill: true,
+    //       data: [120, 230, 130, 440, 250, 360, 270, 180, 90, 300, 310, 220],
+    //       maxBarThickness: 6
 
-        }],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            callbacks: {
-              title: function(context) {
-                const fullMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                return fullMonths[context[0].dataIndex];
-              }
-            }
-          }
-        },
-        interaction: {
-          intersect: false,
-          mode: 'index',
-        },
-        scales: {
-          y: {
-            grid: {
-              drawBorder: false,
-              display: true,
-              drawOnChartArea: true,
-              drawTicks: false,
-              borderDash: [4, 4],
-              color: '#e5e5e5'
-            },
-            ticks: {
-              display: true,
-              color: '#737373',
-              padding: 10,
-              font: {
-                size: 12,
-                lineHeight: 2
-              },
-            }
-          },
-          x: {
-            grid: {
-              drawBorder: false,
-              display: false,
-              drawOnChartArea: false,
-              drawTicks: false,
-              borderDash: [5, 5]
-            },
-            ticks: {
-              display: true,
-              color: '#737373',
-              padding: 10,
-              font: {
-                size: 12,
-                lineHeight: 2
-              },
-            }
-          },
-        },
-      },
-    });
+    //     }],
+    //   },
+    //   options: {
+    //     responsive: true,
+    //     maintainAspectRatio: false,
+    //     plugins: {
+    //       legend: {
+    //         display: false,
+    //       },
+    //       tooltip: {
+    //         callbacks: {
+    //           title: function(context) {
+    //             const fullMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    //             return fullMonths[context[0].dataIndex];
+    //           }
+    //         }
+    //       }
+    //     },
+    //     interaction: {
+    //       intersect: false,
+    //       mode: 'index',
+    //     },
+    //     scales: {
+    //       y: {
+    //         grid: {
+    //           drawBorder: false,
+    //           display: true,
+    //           drawOnChartArea: true,
+    //           drawTicks: false,
+    //           borderDash: [4, 4],
+    //           color: '#e5e5e5'
+    //         },
+    //         ticks: {
+    //           display: true,
+    //           color: '#737373',
+    //           padding: 10,
+    //           font: {
+    //             size: 12,
+    //             lineHeight: 2
+    //           },
+    //         }
+    //       },
+    //       x: {
+    //         grid: {
+    //           drawBorder: false,
+    //           display: false,
+    //           drawOnChartArea: false,
+    //           drawTicks: false,
+    //           borderDash: [5, 5]
+    //         },
+    //         ticks: {
+    //           display: true,
+    //           color: '#737373',
+    //           padding: 10,
+    //           font: {
+    //             size: 12,
+    //             lineHeight: 2
+    //           },
+    //         }
+    //       },
+    //     },
+    //   },
+    // });
 
     var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
 
@@ -383,4 +383,4 @@
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="{{ asset('material/js/material-dashboard.min.js?v=3.2.0')}}"></script>
+  <script src="{{ asset('material/js/material-dashboard.js')}}"></script> 
